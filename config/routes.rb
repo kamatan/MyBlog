31d21035df09b6aa0a0e4e1b2cd3136f93a82f68
kamatan <maron.252548@gmail.com>
2017-08-29 11:04:25 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :messages
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root 'messages#index'
+ resources :messages
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+root
